83b125372400e5469cf2ff639c188539b982c3c7
kalmaske <kalmaske@uncc.edu>
2017-09-11 14:34:22 -0400

    end of day

--- public/app.js
+++ public/app.js
@@ -10,7 +10,6 @@ var config = {
     messagingSenderId: "412484960663"
   };
   firebase.initializeApp(config);
-
 //   Get elements
 const txtEmail = document.getElementById('txtEmail');
 const txtPassword = document.getElementById('txtPassword');
@@ -24,7 +23,36 @@ btnLogin.addEventListener('click', e=> {
     const email = txtEmail.value;
     const pass = txtPassword.value;
     const auth = firebase.auth();
-    
-})
+    // Sign in
+    const promise = auth.signInWithEmailAndPassword(email, pass);
+    promise.catch(e => console.log(e.message));
+});
+
+// Add signup event
+btnSignUp.addEventListener('click', e => {
+    // Get email and pass
+    const email = txtEmail.value;
+    const pass = txtPassword.value;
+    const auth = firebase.auth();
+    // Sign in
+    const promise = auth.createUserWithEmailandPassword(email, pass);
+    promise
+    .catch(e => console.log(e.message));
+});
+
+btnLogout.addEventListener('click', e => {
+    firebase.auth().signOut();
+});
+
+// Add a realtime listener
+firebase.auth().onAuthStateChanged(firebaseUser => {
+     if(firebaseUser) {
+         console.log(firebaseUser);
+         btnLogout.classList.remove('hide');
+    } else {
+        console.log('not logged in');
+        btnLogout.classList.remove('hide');
+    }
+});
 
 }());
