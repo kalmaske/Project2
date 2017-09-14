471e26b8df883474d3d4925feea85479884ecb6f
kalmaske <kalmaske@uncc.edu>
2017-09-14 15:10:01 -0400

    ..

--- config/connection.js
+++ config/connection.js
@@ -9,7 +9,7 @@ if (process.env.JAWSDB_URL) {
         root: 3000,
         host: 'localhost',
         user: 'root',
-        password: 'root',
+        password: 'Newchapter_2017',
         database: 'asheville_db',
     });
 };
