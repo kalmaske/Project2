72d71861aa74eec119acc065219d38feddeb46f8
Graham Lawing <graham.lawing@gmail.com>
2017-09-15 23:02:49 -0400

    jeff changes

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,43 +1,72 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var db = require('../models');
+var db = require("../models");
 
-router.get('/', function (req, res) {
-	res.redirect('/asheville')
+router.get("/", function(req, res) {
+  res.redirect("/asheville");
 });
 
-router.get('/asheville', function (req, res) {
-	db.asheville.findAll({})
-		.then(function (data) {
-			var hbsObject = {
-				listings: data
-			};
+router.get("/asheville", function(req, res) {
+  db.ashevilles.findAll({}).then(function(data) {
+    var hbsObject = {
+      ashevilles: data
+    };
 
-			console.log(hbsObject);
+    console.log(hbsObject);
 
-			res.render('index', hbsObject);
-		});
+    res.render("index", hbsObject);
+  });
 });
 
-router.post('/asheville/create', function (req, res) {
+router.get("/home", function(req, res) {
+  db.ashevilles.findAll({}).then(function(data) {
+    var hbsObject = {
+      user: req.user,
+      ashevilles: data
+    };
 
-	db.listings.create(['name'], [req.body.name], function (data) {
-		res.redirect('/')
-	});
+    console.log(hbsObject);
+
+    res.render("home", hbsObject);
+  });
 });
 
-router.put('/asheville/update/:id', function (req, res) {
-	var condition = 'id = ' + req.params.id;
+router.post("/asheville/create", function(req, res) {
+  var listing = {
+    name: req.body.name,
+    neighbourhood: req.body.neighbourhood,
+    room_type: req.body.room_type,
+    price: req.body.price,
+    minimum_nights: req.body.minimum_nights,
+    availability_365: req.body.availability_365
+  };
 
-	console.log('condition ', condition);
+  console.log(listing);
 
-	db.listings.update({
-		'name': req.body.name
-	}, condition, function (data) {
-		res.redirect('/asheville');
-	});
+  db.ashevilles
+    .create(listing)
+    .then(function() {
+      res.redirect("/");
+    })
+    .catch(function(err) {
+      console.error(err);
+    });
 });
 
+router.put("/asheville/update/:id", function(req, res) {
+  var condition = "id = " + req.params.id;
 
+  console.log("condition ", condition);
+
+  db.ashevilles.update(
+    {
+      name: req.body.name
+    },
+    condition,
+    function(data) {
+      res.redirect("/asheville");
+    }
+  );
+});
 
 module.exports = router;
